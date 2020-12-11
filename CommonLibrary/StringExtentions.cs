7decd9f5b535ebf1ed5434e20d718e598f43481c
LokeshAcharB <lokeshachar.b2@gmail.com>
2020-12-11 15:22:36 +0530

    Add files via upload

---
+++ CommonLibrary/StringExtentions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CommonLibrary
+{
+    public static class StringExtentions
+    {
+        private readonly static Regex rex = new Regex("^[a-zA-Z0-9-]*$");
+        public static bool IsValidPUID(this string src) => src.Length == 4 && rex.IsMatch(src) ? true : false;
+        public static bool IsAlphaNumeric(this string src) => rex.IsMatch(src) ? true : false;
+    }
+}
